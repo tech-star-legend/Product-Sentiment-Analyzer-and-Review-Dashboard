9f3d60b6629096669b83eb2cba364be59ccc3ee3
tech-star-legend <technicalsugumar@gmail.com>
2026-05-07 20:55:39 +0530

    Add files via upload

---
+++ backend/scraper.py
@@ -0,0 +1,9 @@
+def scrape_reviews(product_name):
+    # Dummy reviews (replace with Selenium later)
+    return [
+        "This product is amazing",
+        "Worst quality ever",
+        "It is okay not great",
+        "Loved it",
+        "Very bad experience"
+    ]
